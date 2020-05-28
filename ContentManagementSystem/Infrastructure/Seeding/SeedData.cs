83d1cc6725a02a0ba5ff578f573922b3873c6468
Murat Sivil <63340228+muratsivil@users.noreply.github.com>
2020-05-28 14:17:30 +0300

    I created a class named "SeedData.cs" and I added three data into the SeedData.cs

---
+++ ContentManagementSystem/Infrastructure/Seeding/SeedData.cs
@@ -0,0 +1,59 @@
+﻿using ContentManagementSystem.Infrastructure.Context;
+using ContentManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContentManagementSystem.Infrastructure.Seeding
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ProjectContext(serviceProvider.GetRequiredService<DbContextOptions<ProjectContext>>()))
+            {
+                if (context.Pages.Any())
+                {
+                    return;
+                }
+                else
+                {
+                    context.Pages.AddRange(
+                        new Page
+                        {
+                            Title = "Home",
+                            Slug = "home",
+                            Content = "home page",
+                            Sorting = 0,
+                        },
+                        new Page
+                        {
+                            Title = "About Us",
+                            Slug = "about-us",
+                            Content = "about us page",
+                            Sorting = 500,
+                        },
+                        new Page
+                        {
+                            Title = "Services",
+                            Slug = "services",
+                            Content = "services page",
+                            Sorting = 500,
+                        },
+                        new Page
+                        {
+                            Title = "Contact",
+                            Slug = "contact",
+                            Content = "contact page",
+                            Sorting = 500,
+                        }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,8 +1,16 @@
 
+---------------------------------------README---------------------------------------
 
 1. Download these packages by using Package NuGet Manager;
 	1.1. Download Microsoft.EntityFrameworkCore.SqlServer
 	1.2. Download Microsoft.EntityFrameworkCore.Tools
 2. Open a Folder named "Infrastructure"
 	2.1. Open a Folder named "Context"
-		2.1.1. Add
+		2.1.1. Add a class named "ProjectContext.cs"
+3. Add a class named "Page.cs" into "Model" Folder.
+4. Do DbSet for "Page" entity.
+5. Migration processes will start.
+6. Open a Folder named "Seeding" into Infrastructure folder.
+	6.1. Add a class named "SeedData.cs"
+	6.2. Organize the Program.cs
+7. Start the Project and check the database.

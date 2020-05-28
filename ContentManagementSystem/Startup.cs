c6ef2126a1889f15b029e04a1d5ceeb24d7bceb6
Murat Sivil <63340228+muratsivil@users.noreply.github.com>
2020-05-28 12:49:50 +0300

    Installed sqlserver and tools packages and esablish the connection to database.

--- ContentManagementSystem/Startup.cs
+++ ContentManagementSystem/Startup.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContentManagementSystem.Infrastructure.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,8 @@ namespace ContentManagementSystem
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ProjectContext>(options => options.UseSqlServer
+            (Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+
+
+1. Download these packages by using Package NuGet Manager;
+	1.1. Download Microsoft.EntityFrameworkCore.SqlServer
+	1.2. Download Microsoft.EntityFrameworkCore.Tools
+2. Open a Folder named "Infrastructure"
+	2.1. Open a Folder named "Context"
+		2.1.1. Add
